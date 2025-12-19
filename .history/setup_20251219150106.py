fb17e5d930ff3f469526d556b0cf18a0b5aed405
Evanaxander <abir.ehsan11@gmail.com>
2025-12-19 15:47:47 +0600

    requirements added

--- .history/setup_20251219150106.py
+++ .history/setup_20251219150106.py
@@ -4,7 +4,7 @@ setup(
     name="medical_chatbot",
     version="0.1.0",
     author="Abir Ehsan Evan",
-    author_email="abir.ehsan11@gmail.com",
+    author_email="abir.ehsan123@gmail.com",
     packages=find_packages(),
     install_requires=[]
 )
